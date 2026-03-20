eccd0bcb01b415549d9866c8fa4bc237d09a893d
ginuraMA <manethginura@gmail.com>
2026-03-20 04:17:33 +0300

    revision_3

--- Chapter_5/num_to_words.c
+++ Chapter_5/num_to_words.c
@@ -2,7 +2,9 @@
 
 int main(void)
 {
-    int digit, num;
+    int digit, num, j;
+    int arr[100];
+    int i = 0;
 
     printf("Enter the digit\n");
     scanf("%i", &digit);
@@ -10,29 +12,34 @@ int main(void)
     while (digit != 0)
     {
         num = digit % 10;
-        if (num == 0)
+        arr[i] = num;
+        i++;
+        digit = digit / 10;    
+    }
+
+    for (j = i - 1; j > -1; j--){
+        if (arr[j] == 0)
             printf("zero ");
-        else if (num == 1)
+        else if (arr[j] == 1)
             printf("one ");
-        else if (num == 2)
+        else if (arr[j] == 2)
             printf("two ");
-        else if (num == 3)
+        else if (arr[j] == 3)
             printf("three ");
-        else if (num == 4)
+        else if (arr[j] == 4)
             printf("four ");
-        else if (num == 5)
+        else if (arr[j] == 5)
             printf("five ");
-        else if (num == 6)
+        else if (arr[j] == 6)
             printf("six ");
-        else if (num == 7)
+        else if (arr[j] == 7)
             printf("seven ");
-        else if (num == 8)
+        else if (arr[j] == 8)
             printf("eight ");
-        else if (num == 9)
+        else if (arr[j] == 9)
             printf("nine ");
         else
-            printf("Invalid number\n");  
-        digit = digit / 10;    
+            printf("Invalid number\n"); 
     }
     printf("\n");
     return 0;
