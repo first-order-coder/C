cd161ec22c85b64abaac95e2f19db9fa296847e0
ginuraMA <manethginura@gmail.com>
2026-03-29 10:12:16 +0300

    revision_6

--- Chapter_8/excer_2.c
+++ Chapter_8/excer_2.c
@@ -26,16 +26,10 @@ int g_month(struct date d){
 
 int calcNValue(struct date d){ //every function must return something unless expilicitly mentioned void
 
-    // int year = function_date_year(d);
-    // int month = g_month(d);
-
-    // printf("%i %i\n", year, month);
-    
     int N = ((1461 * function_date_year(d)) / 4)  + ((153 * g_month(d)) / 5) + d.day;
 
     return N;
 }
-
 int main(void)
 {
     struct date dateTime1, dateTime2;
@@ -44,7 +38,23 @@ int main(void)
     scanf("%d%d%d %d%d%d", &dateTime1.month, &dateTime1.day, &dateTime1.year, &dateTime2.month, &dateTime2.day, &dateTime2.year);
 
     int N1 = calcNValue(dateTime1);
-    printf("%i\n", N1);
+    int N2 = calcNValue(dateTime2);
+
+    const char days[7][10] = { //this or const char *days[] = 
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday"
+    };
+
+    int index = (N1 - 621049) % 7;
+    const char *dayOfTheWeek = days[index];
+
+    printf("Number of days between is: %i\n", (N2 - N1));
+    printf("Day of the week is: %s\n", dayOfTheWeek);
 
     return 0;
 }
