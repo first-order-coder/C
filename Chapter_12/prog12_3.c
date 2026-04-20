54e438fe744162017723964320ef183ad83ac89f
ginuraMA <manethginura@gmail.com>
2026-04-20 22:18:42 +0300

    revision_16

---
+++ Chapter_12/prog12_3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "metric.h"
+
+int main(void)
+{
+    float liters, gallons;
+
+    printf("*** Liters to gallons ***\n\n");
+    printf("Enter the number of liters: ");
+    scanf("%f", &liters);
+
+    gallons = liters * QUARTS_PER_LITER / 4.0;
+    printf("%g liters = %g gallons\n", liters, gallons);
+
+    return 0;
+}
